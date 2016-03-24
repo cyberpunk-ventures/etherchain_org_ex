89af6bd02cb7ee9696d0fae789087f315cdf2cac
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2016-03-24 21:56:18 +0200

    Added account_tx function and tests

--- lib/etherchain_org.ex
+++ lib/etherchain_org.ex
@@ -28,7 +28,7 @@ defmodule EtherchainOrg do
   end
 
   @doc """
-    Function returns a map with the following structure:
+    Function returns a map with the following schema:
 
     %{
       address: "0x32be343b94f860124dc4fee278fdcbd38c102d88",
@@ -44,12 +44,42 @@ defmodule EtherchainOrg do
     %{"data" => data} = response.body |> Poison.Parser.parse!
     data
     |> hd
+    |> convert_string_keys_to_atom
+  end
+
+  @doc """
+  https://etherchain.org/documentation/api/#api-Accounts-GetAccountIdTxOffset
+  Returns a list of transactions maps with the schema matching the following example:
+    {
+         "hash": "0xa04966d391d6e3499ab1cab91b1db7723567025a3b64bace7eceec98403241c4",
+         "sender": "0xb794f5ea0ba39494ce839613fffba74279579268",
+         "recipient": "0x32be343b94f860124dc4fee278fdcbd38c102d88",
+         "accountNonce": "33",
+         "price": 60000000000,
+         "gasLimit": 21000,
+         "amount": 5e+22,
+         "block_id": 1201932,
+         "time": "2016-03-23T10:48:37.000Z",
+         "newContract": 0,
+         "isContractTx": null,
+         "blockHash": "0xc4724a224a90aa843f0c06f5f66b69262fb9eab0d81bc3a101ae9f66f7fe4b52",
+         "parentHash": "0xa04966d391d6e3499ab1cab91b1db7723567025a3b64bace7eceec98403241c4",
+         "txIndex": null
+     }
+  """
+  def account_tx(id, offset) when is_integer(offset) and is_bitstring(id) do
+    {:ok, response} = EtherchainOrg.get "/account/#{id}/tx/#{offset}"
+    %{"data" => data} = response.body |> Poison.Parser.parse!
+    data
+    |> Enum.map(&convert_string_keys_to_atom/1)
+  end
+
+  defp convert_string_keys_to_atom(m) do
+    m
     |> Enum.map(fn {k,v} -> {String.to_atom(k),v} end)
     |> Enum.reduce(%{}, fn {k,v},acc ->
       Map.merge(acc, %{k => v})
     end)
-
   end
-
 
 end
--- test/etherchain_org_test.exs
+++ test/etherchain_org_test.exs
@@ -11,4 +11,10 @@ defmodule EtherchainOrgTest do
     account_data = EtherchainOrg.account(id) |> Map.to_list
     assert Vex.valid? account_data, address: [presence: true], balance: [presence: true]
   end
+
+  test "returns data about account transactions" do
+    id = "0xb794F5eA0ba39494cE839613fffBA74279579268" # account with most ether on balance
+    account_data = EtherchainOrg.account_tx(id, 0) |> hd |> Map.to_list
+    assert assert Vex.valid? account_data, hash: [presence: true], sender: [presence: true], recipient: [presence: true]
+  end
 end
