4c6fafd20a5f6024822bc91ff8748df5b09afe45
Vitalii Daniuk <vitalii.daniuk@gmail.com>
2016-03-29 16:25:47 +0300

    test refactor

--- test/etherchain_org_test.exs
+++ test/etherchain_org_test.exs
@@ -3,18 +3,21 @@ defmodule EtherchainOrgTest do
   doctest EtherchainOrg
 
   test "correctly returns number of ether tokens" do
-    assert EtherchainOrg.supply >= 70_000_000
+    {:ok, supply} = EtherchainOrg.supply
+    assert supply  >= 70_000_000
   end
 
   test "correctly returns data about account" do
     id = "0xb794F5eA0ba39494cE839613fffBA74279579268" # account with most ether on balance
-    account_data = EtherchainOrg.account(id) |> Map.to_list
+    {:ok, response} = EtherchainOrg.account(id)
+    account_data = response |> Map.to_list
     assert Vex.valid? account_data, address: [presence: true], balance: [presence: true]
   end
 
   test "returns data about account transactions" do
     id = "0xb794F5eA0ba39494cE839613fffBA74279579268" # account with most ether on balance
-    account_data = EtherchainOrg.account_tx(id, 0) |> hd |> Map.to_list
+    {:ok, response} = EtherchainOrg.account_tx(id, 0)
+    account_data = response  |> hd |> Map.to_list
     assert assert Vex.valid? account_data, hash: [presence: true], sender: [presence: true], recipient: [presence: true]
   end
 end
